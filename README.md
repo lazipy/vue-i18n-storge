9a7a3395d1d18c367153b5279b9838704e1d7ebf
李子屏 <877286986@qq.com>
2019-07-24 09:13:21 +0800

    Commit:vue-i18n-storge

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# vue-i18n-storge
-Internationalize the cache approximately and internationalize it
+# vue-router-dispense
+
+一个基于 vue-i18n 的扩展， vue-i18n 原有功能及配置完全保留，主要功能有以下几点：
+
+1. 指定语言包配置模块，统一加载，无需单个 import 进来
+2. 当前语言缓存cookie
+3. 提供setLocale()和getlLocale()函数
+
+#### 安装
+
+```
+npm install vue-i18n-storge -S
+```
+
+#### 使用
+
+```
+import Vue from 'vue'
+import VueI18nObServer from '../libs/vue-i18n-observer'
+
+Vue.use(VueI18nObServer)
+
+const i18n = new VueI18nObServer({
+  default: 'zh-CN',
+  files: require.context('../locales', true, /\.js$/)
+})
+
+export default i18n
+```
+
+##### default 
+
+指定默认的语言
+
+##### files
+
+语言包的路径
+
+##### 使用 i18n.locale = 'en-US' 和 i18n.setLocale('en-US') 的区别
+
+i18n.setLocale 会缓存到 cookie ，并且会刷新页面
+
+i18n.locale = 'en-US' 不会缓存到 cookie，不会刷新页面
---
+++ dist/index.cjs.js
@@ -0,0 +1,66 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var Vuei18n = _interopDefault(require('vue-i18n'));
+var Cookies = _interopDefault(require('js-cookie'));
+
+/**
+ * 1、配置options自动加载locales文件，支持原有options
+ * 2、setLocale()函数一键更换，
+ * 3、当前语言缓存cookie
+ * 4、data中的多语言支持响应式改变
+ */
+
+let messages = {};
+
+// 获取所有语言包
+function getLocales(files) {
+  files.keys().forEach(key => {
+    const name = key.slice(2, -3);
+    const locale = files(key).default;
+    messages[name] = locale;
+  });
+}
+
+function VueI18nStorge(options) {
+  let locale = Cookies.get('locale');
+  if (!locale) {
+    locale = options.default;
+    Cookies.set('locale', locale);
+  }
+  const files = options.files;
+  getLocales(files); // 获取语言包
+
+  delete options.files;
+  delete options.default;
+
+  let params = Object.assign({ locale, messages }, options);
+
+  let i18n = new Vuei18n(params);
+
+  i18n.setLocale = function (locale) {
+    Cookies.set('locale', locale);
+    window.location.reload();
+  };
+
+  i18n.getLocale = function () {
+    return i18n.locale;
+  };
+
+  // 热更新
+  if (module.hot) {
+    module.hot.accept(files.keys(), function () {
+      files.keys().forEach(key => {
+        const name = key.slice(2, -3);
+        const locale = files(key).default;
+        i18n.setLocaleMessage(name, locale);
+      });
+    });
+  }
+  return i18n;
+}
+
+VueI18nStorge.install = Vuei18n.install;
+
+module.exports = VueI18nStorge;
